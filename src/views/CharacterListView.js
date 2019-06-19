2ea6d1f82dccc57733d8b6961ae59dc616047720
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-20 06:38:57 +1000

    imported action creator, called it within CDM, built out props I wanted to xfer to characterlistview from globalstore/state, connected them

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,14 +3,13 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getData } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
 
   componentDidMount() {
     // call our action
+    this.props.getData();
   }
 
   render() {
@@ -27,9 +26,16 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+const mapStateToProps = state => {
+  return {
+    characters: state.characters,
+    fetching: state.isFetching
+  };
+};
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    getData
   }
 )(CharacterListView);
